f20e4261217dc109254a8352740f90ec45b447df
Viktor Kuzubov <kuzubovvv@gmail.com>
2024-05-07 00:58:22 +0700

    первая стр

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import './index.scss'
 import {Header} from './components/header/header'
+import { MainPage } from './components/mainPage/mainPage'
+
 export default function App(){
     return(
         <main className='wrapConteiner'>
             <Header/>
+            <MainPage/>
         </main>
     )
 }
--- src/components/mainPage/mainPage.js
+++ src/components/mainPage/mainPage.js
@@ -1,5 +1,15 @@
-const MainPage=()=>{
-    <section>
-        
-    </section>
+import styles from './mainPage.module.scss'
+
+export const MainPage=()=>{
+    return(
+        <div className={styles.container}>
+            <div>
+                <h1 className={styles.h1}>СТУДИЯ ЙОГИ И ПИЛАТЕСА</h1>
+            </div>
+            <div >
+                <p className={styles.p}>Здесь мы поможем вам держать тело  в тонусе, а душу - в гармонии.</p>
+            </div>
+        </div>
+    )
+    
 }
