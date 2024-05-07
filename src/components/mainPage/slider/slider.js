26a5d6825063e0f904dbe4406fd1462c972eb5db
Viktor Kuzubov <kuzubovvv@gmail.com>
2024-05-08 01:50:13 +0700

    поправил хом стр

--- src/components/mainPage/slider/slider.js
+++ src/components/mainPage/slider/slider.js
@@ -1,6 +1,7 @@
+import styles from './slider.module.scss'
 export const Slider=()=>{
     return (
-        <div>
+        <div className={styles.container }>
             <img src="/assets/slider-1.svg" alt="slider-1"/>
         </div>
     )
