3ecd9ad997366c73ca717c85b7345e252743f5d9
koteuka201 <kuzubovvv@gmail.com>
2024-05-07 16:47:45 +0700

    главная стр

--- src/components/mainPage/mainPage.js
+++ src/components/mainPage/mainPage.js
@@ -1,14 +1,17 @@
 import styles from './mainPage.module.scss'
-
+import { Slider } from './slider/slider'
 export const MainPage=()=>{
     return(
         <div className={styles.container}>
-            <div>
+            <div className={styles.h1Module}>
                 <h1 className={styles.h1}>СТУДИЯ ЙОГИ И ПИЛАТЕСА</h1>
             </div>
-            <div >
+            <div className={styles.pModule}>
                 <p className={styles.p}>Здесь мы поможем вам держать тело  в тонусе, а душу - в гармонии.</p>
             </div>
+           <div className={styles.slider}>
+                <Slider/>
+           </div>
         </div>
     )
     
---
+++ src/components/mainPage/slider/slider.js
@@ -0,0 +1,7 @@
+export const Slider=()=>{
+    return (
+        <div>
+            <img src="/assets/slider-1.svg" alt="slider-1"/>
+        </div>
+    )
+}
