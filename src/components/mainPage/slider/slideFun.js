a603baa668464ca997166fb9de9320abd832fbd8
Viktor Kuzubov <kuzubovvv@gmail.com>
2024-05-09 03:34:34 +0700

    сделал все со слайдером кроме плавного транз

---
+++ src/components/mainPage/slider/slideFun.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from './slider.module.scss'
+
+export const Slide = ({ src  }) => {
+  return (
+    
+    <img  src={src} alt="Slide"/>
+    
+  );
+};
--- src/components/mainPage/slider/slider.js
+++ src/components/mainPage/slider/slider.js
@@ -1,8 +1,46 @@
+import React, { useState } from 'react';
 import styles from './slider.module.scss'
+import { Slide } from './slideFun'
+
 export const Slider=()=>{
+
+    const images = [
+        '/assets/slider/slider-1.svg',
+        '/assets/slider/slider-2.svg',
+        '/assets/slider/slider-3.svg'
+    ];
+
+    const [currentIndex, setCurrentIndex] = useState(0);
+
+    const goToNextSlide = () => {
+        const newIndex = (currentIndex + 1) % 3;
+        setCurrentIndex(newIndex);
+    };
+
+    const goToPrevSlide = () => {
+        const newIndex = (currentIndex - 1 + 3) % 3;
+        setCurrentIndex(newIndex);
+    };
+
     return (
         <div className={styles.container }>
-            <img src="/assets/slider-1.svg" alt="slider-1"/>
+            <div>
+                <Slide src={images[currentIndex]} />
+            </div>
+            <div className={styles.switchContainer}>
+                <div className={styles.indexContainer}>
+                    {currentIndex+1}/3
+                </div>
+                
+                <div className={styles.progressContainer}>
+                    <div
+                    className={styles.progressBar}
+                    style={{ width: `${((currentIndex + 1) / images.length) * 100}%` }}
+                    />
+                </div>
+                <img onClick={goToPrevSlide} style={{ marginRight: '8px',marginLeft: '26px' }} src="/assets/arrows/leftArrow.svg" alt="leftArrow" />
+                <img onClick={goToNextSlide} src="/assets/arrows/rightArrow.svg" alt="rightArrow" />
+            </div>            
         </div>
     )
 }
