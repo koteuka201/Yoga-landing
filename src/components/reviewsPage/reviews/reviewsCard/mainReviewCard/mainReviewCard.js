de2aacc68a1dc7019ed8e4a90c6938528b3ed1b3
Viktor Kuzubov <kuzubovvv@gmail.com>
2024-05-18 15:25:19 +0700

    пофиксил отображение картинок на стр Отзывы

--- src/components/reviewsPage/reviews/reviewsCard/mainReviewCard/mainReviewCard.js
+++ src/components/reviewsPage/reviews/reviewsCard/mainReviewCard/mainReviewCard.js
@@ -6,13 +6,13 @@ export const MainReviewCard=({name, text, date, img})=>{
 
         
         <div className={styles.card}>
-            <div className={styles.cutout}>
+            {/* <div className={styles.cutout}>
                 
-            </div>
-            {/* <div 
-                className={styles.cutoutImg}
-                style={{ backgroundImage: `url(${img})` }}>
             </div> */}
+            <div 
+                className={styles.cutout}
+                style={{ backgroundImage: `url(${img})` }}>
+            </div>
             <div className={styles.content}>
                 <div className={styles.name}>{name}</div>
                 <div className={styles.line}></div>
--- src/components/reviewsPage/reviews/reviewsCard/sideReviewCard/sideReviewCard.js
+++ src/components/reviewsPage/reviews/reviewsCard/sideReviewCard/sideReviewCard.js
@@ -10,13 +10,11 @@ export const SideReviewCard=({name, text, date, img, more})=>{
     }
     return(
         <div className={styles.card}>
-            <div className={styles.cutout}>
-                
-            </div>
-            {/* <div 
-                className={styles.cutoutImg}
+            
+            <div 
+                className={styles.cutout}
                 style={{ backgroundImage: `url(${img})` }}>
-            </div> */}
+            </div>
             <div className={styles.content}>
                 <div className={styles.name}>{name}</div>
                 <div className={styles.line}></div>
