00f0b19892480c419124d2e13e72fab751f8e9b0
Viktor Kuzubov <kuzubovvv@gmail.com>
2024-05-22 16:56:14 +0700

    доделал модалку, а также дорасставил цветочки

--- src/components/reviewsPage/reviews/reviewsCard/mainReviewCard/mainReviewCard.js
+++ src/components/reviewsPage/reviews/reviewsCard/mainReviewCard/mainReviewCard.js
@@ -5,25 +5,31 @@ export const MainReviewCard=({name, text, date, img})=>{
         <div className={styles.container}>
 
         
-        <div className={styles.card}>
-            {/* <div className={styles.cutout}>
-                
-            </div> */}
-            <div 
-                className={styles.cutout}
-                style={{ backgroundImage: `url(${img})` }}>
+            <div className={styles.card}>
+                {/* <div className={styles.cutout}>
+                    
+                </div> */}
+                <div 
+                    className={styles.cutout}
+                    style={{ backgroundImage: `url(${img})` }}>
+                </div>
+                <div className={styles.content}>
+                    <div className={styles.name}>{name}</div>
+                    <div className={styles.line}></div>
+                    <div className={styles.text}>{text}</div>
+                    
+                </div>
+                <div className={styles.footer}>
+                    <div className={styles.more}>Подробней</div>
+                    <div className={styles.date}>{date}</div>
+                </div>
+                <div
+                    className={styles.flower}
+                    style={{backgroundImage: `url('/assets/flowers/reviewFlower.svg')`}}
+                >
+
+                </div>
             </div>
-            <div className={styles.content}>
-                <div className={styles.name}>{name}</div>
-                <div className={styles.line}></div>
-                <div className={styles.text}>{text}</div>
-                
-            </div>
-            <div className={styles.footer}>
-                <div className={styles.more}>Подробней</div>
-                <div className={styles.date}>{date}</div>
-            </div>
-        </div>
         </div>
     )
 }
--- src/components/shared/modal/modal.js
+++ src/components/shared/modal/modal.js
@@ -1,11 +1,29 @@
-import React from "react";
+import React,{useEffect, useState} from "react";
 import ReactDOM from 'react-dom';
+import { useForm } from "react-hook-form";
 import styles from './modal.module.scss'
 
+
 export const Modal = ({ isOpen, onClose, direction, time }) => {
-    if (!isOpen) return null;
-    
+
+    const {register, handleSubmit, reset, formState: {errors}}=useForm()
+    const [isSubmit,setIsSubmit]=useState(false)
+
+    useEffect(() => {
+        if (!isOpen) {
+            setIsSubmit(false)
+            reset();
+        }
+    }, [isOpen, reset]);
+
+    if (!isOpen) return null
+
     const placeholder=`${direction} (${time})`
+
+    const onSubmit=()=>{
+        setIsSubmit(true)
+    }
+
     return ReactDOM.createPortal(
       <div className={styles.overlay} onClick={onClose}>
         <div className={styles.modal} onClick={(e) => e.stopPropagation()}>
@@ -16,25 +34,63 @@ export const Modal = ({ isOpen, onClose, direction, time }) => {
                 </svg>
             </button>
             </div>
-            <div className={styles.h1Module}>
-                <div className={styles.h1}>
-                    Записаться на тренировку
-                </div>
-            </div>
-            <form className={styles.formModule}>
-                <input className={styles.dir} type="text" placeholder="Направление" value={placeholder} disabled />
-                <input className={styles.phone} type="number" placeholder="Номер телефона"/>
-                <input className={styles.comment} type="text" placeholder="Комментарий"/>
-            </form>
-            <div className={styles.btnModule}>
-                <button className={styles.btn}>
-                    ОТПРАВИТЬ
-                </button>
-            </div>
-            <div className={styles.policyModule}>
-                <input style={{width: '16px', height: '16px', alignSelf: 'center', cursor: 'pointer'}} type="checkbox" />
-                <span className={styles.policy}>принимаю условия политики конфиденциальности</span>
-            </div>
+            {isSubmit ? (
+                <>
+                    <div className={styles.h1CompleteModule}>
+                        <div className={styles.h1}>
+                            спасибо! <br/>
+                            ваша заявка принята
+                        </div>
+                    </div>
+                    <div className={styles.p1Module}>
+                        <div className={styles.p1}>
+                            Наш менеджер свяжется с вами в ближайшее время!
+                        </div>
+                    </div>
+                </>
+            ) : (    
+                    <>
+                
+                        <div className={styles.h1Module}>
+                            <div className={styles.h1}>
+                                Записаться на тренировку
+                            </div>
+                        </div>
+                        <form className={styles.formModule} onSubmit={handleSubmit(onSubmit)}>
+                            <div style={{paddingLeft: '10px', paddingRight: '10px'}}>
+                                <input className={styles.dir} type="text" placeholder="Направление" value={placeholder} disabled />
+                                <input 
+                                    className={styles.phone} 
+                                    type="tel" 
+                                    placeholder="Номер телефона"
+                                    {...register('phone',{
+                                        required: 'Номер телефона обязателен',
+                                        pattern:{
+                                            value: /^\+7\d{10}$/,
+                                            message: 'Используйте формат номера телефона: +7xxxxxxxxxx'
+                                        }
+                                    })}
+                                />
+                                {errors.phone && <p className={styles.error}>{errors.phone.message}</p>}
+                                <input className={styles.comment} type="text" placeholder="Комментарий"/>
+                            </div>
+                            <button type="submit" className={styles.btn}>
+                                ОТПРАВИТЬ
+                            </button>
+                            <div className={styles.policyModule}>
+                                <input 
+                                    className={styles.checkbox}
+                                    type="checkbox"
+                                    {...register('policy',{
+                                        required: 'Вы должны принять условия пол'
+                                    })}
+                                />
+                                <span className={!errors.policy ? styles.policy : styles.policyError}>принимаю условия политики конфиденциальности</span>
+                            </div>
+                        </form>
+                    </>
+                )
+            }
         </div>
       </div>,
       document.getElementById('portal')
